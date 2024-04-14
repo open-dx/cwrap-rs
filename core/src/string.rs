f0e99f019f9f32fb19c9dcd1a832d6cc56a5e34f
Lorren Biffin <lorren.biffin@gmail.com>
2024-04-14 16:30:58 -0700

    TIFU; Keep unsafe out of the public api.

--- core/src/string.rs
+++ core/src/string.rs
@@ -4,13 +4,15 @@ use crate::error::CStringError;
 
 
 /// TODO
-pub unsafe fn try_unwrap_cstr<'out>(bytes: *const i8) -> Result<&'out str, CStringError> {
+pub fn try_unwrap_cstr<'out>(bytes: *const i8) -> Result<&'out str, CStringError> {
     if bytes.is_null() {
         return Err(CStringError::Uninitialized);
     }
     
-    match CStr::from_ptr(bytes).to_str() {
-        Ok(c_str) => Ok(c_str),
-        Err(error) => Err(CStringError::Utf8Error(error)),
+    unsafe {
+        match CStr::from_ptr(bytes).to_str() {
+            Ok(c_str) => Ok(c_str),
+            Err(error) => Err(CStringError::Utf8Error(error)),
+        }
     }
 }
