a213027873e9c8bb491c18216a30b12246ced46d
Lorren Biffin <lorren.biffin@gmail.com>
2024-05-09 15:33:01 -0700

    Added NulError to CStringError

--- core/src/error.rs
+++ core/src/error.rs
@@ -16,7 +16,7 @@ pub fn report_err<E: std::fmt::Display>(error: E) {
 #[derive(oops::Error)]
 pub enum CBindingError {
     /// TODO
-    #[msg = "c-binding error: {:}"]
+    #[msg = "c-binding error: {0:}"]
     CStringError(CStringError),
 }
 
@@ -28,10 +28,17 @@ pub enum CStringError {
     Uninitialized,
 
     /// TODO
-    #[msg = "utf-8 error: {:}"]
+    #[msg = "utf-8 error: {0:}"]
     Utf8Error(Utf8Error),
 
+    /// TODO: See how/if we can do this without the std lib.
+    #[msg = "null string: {0:}"]
+    NulError(std::ffi::NulError),
+}
+
+impl From<std::ffi::NulError> for CStringError {
     /// TODO
-    #[msg = "null string"]
-    NulError,
+    fn from(error: std::ffi::NulError) -> Self {
+        CStringError::NulError(error)
+    }
 }
