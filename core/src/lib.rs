90dfa948a805f5d451fe5c30b8d0e1dd7e375b36
Lorren Biffin <lorren.biffin@gmail.com>
2024-05-09 16:07:47 -0700

    Added initial path module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,4 +5,6 @@ pub mod error;
 
 pub mod string;
 
+pub mod path;
+
 pub mod drop;
---
+++ core/src/path.rs
@@ -0,0 +1,18 @@
+use std::path::Path;
+use std::path::PathBuf;
+use std::ffi::CString;
+
+use crate::error::CStringError;
+
+
+/// TODO
+pub fn try_path_from_cstring(path_cstring: CString) -> Result<PathBuf, CStringError> {
+    let path_str = crate::string::try_unwrap_cstr(path_cstring.as_ptr())?;
+    Ok(PathBuf::from(path_str))
+}
+
+/// TODO
+pub fn try_cstring_from_path(path: &Path) -> Result<CString, CStringError> {
+    let path_str = path.as_os_str().to_str().unwrap_or_default();
+    CString::new(path_str).map_err(CStringError::from)
+}
